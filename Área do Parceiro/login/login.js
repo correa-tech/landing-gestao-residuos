279615adbd2ad6b5460435df1d75d5e28f7ad426
correa-tech <guilhermeluis899@gmail.com>
2025-07-04 02:26:56 -0300

    feature: refatorei a pagina inteira de login e finalizei todas as funcionalidades

--- Área do Parceiro/login/login.js
+++ Área do Parceiro/login/login.js
@@ -22,10 +22,15 @@ form.addEventListener('input', function () {
 
   if (todosPreenchidos) {
     const email = document.getElementById('email').value;
-    localStorage.setItem('email', email);
-    btn.removeAttribute('disabled');
-    btn.classList.remove('botao-desabilitado');
-    btn.classList.add('botao-habilitado');
+      if(!email.endsWith('@gmail.com')){
+      todosPreenchidos = false;
+      alert('O email deve terminar com "@"!!')
+      }else{
+        localStorage.setItem('email', email);
+        btn.removeAttribute('disabled');
+        btn.classList.remove('botao-desabilitado');
+        btn.classList.add('botao-habilitado');
+      }
 
   } else {
     btn.setAttribute('disabled', true);
@@ -45,11 +50,38 @@ btn.addEventListener('click', function (event) {
 });
 
 const icone = document.getElementById('icone-login');
-const divTeste = document.getElementById('divTest');
-icone.addEventListener('click', function(){
-  if(divTeste.classList.contains('divTESTE')){
+const imagem = document.getElementById('imagem-sustentavel');
+const abaParceiros = document.getElementById('aba-parceiros');
+const btnParceiros = document.getElementById('botao-parceiros');
 
-    divTeste.classList.remove('divTESTE');
-    divTeste.classList.add('teste');
-  }
-})
+btnParceiros.addEventListener('click', () => {
+  abaParceiros.classList.remove('animate-div'); // remove a animação de entrada
+  abaParceiros.classList.add('animate-div-reverse'); // adiciona a de saída
+
+  setTimeout(() => {
+    abaParceiros.style.display = 'none';
+    abaParceiros.classList.remove('animate-div-reverse'); // limpa após esconder
+  }, 2300); // igual ao tempo de animação no CSS
+});
+
+
+
+icone.addEventListener('click', () => {
+  abaParceiros.style.display = 'flex';
+  abaParceiros.classList.remove('animate-div-reverse'); // limpa reverso
+  void abaParceiros.offsetWidth; // reflow
+  abaParceiros.classList.add('animate-div'); // aplica entrada
+});
+
+const image = document.getElementById('fundo');
+
+document.addEventListener('mousemove', (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 30; // de -15px a +15px
+  const y = (e.clientY / window.innerHeight - 0.5) * 30;
+
+  image.style.transform = `translate(calc(-50% + ${x}px), calc(-50% + ${y}px))`;
+});
+
+
+
+
