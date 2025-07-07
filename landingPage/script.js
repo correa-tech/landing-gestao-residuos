54dda2aa10492a9c0e3f995ddb3fef654b047342
Franciane SL <fran.schier@outlook.com>
2025-07-06 21:53:47 -0300

    feat: Implementação de destaque dos itens do menu ao passar o mouse.

--- landingPage/script.js
+++ landingPage/script.js
@@ -2,6 +2,20 @@ const cabecalho = document.getElementById('cabecalho');
 const itensMenu = document.querySelectorAll('#menu li');
 
 window.addEventListener("load", () => {
+
     cabecalho.style.opacity = 1;
     
+})
+
+itensMenu.forEach((item) => {
+
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "#ffffff";
+        item.style.color = "#71A53C";
+    });
+
+    item.addEventListener("mouseout",() => {
+        item.style.backgroundColor = "transparent";
+        item.style.color = "#ffffff";
+    });
 })
